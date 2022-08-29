76b742c31bcdf4d46416f14a88f43041da5cf9c0
idonia27 <111659728+idonia27@users.noreply.github.com>
2022-08-29 16:48:45 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -9,5 +9,8 @@ int main(int argc, char **argv) {
     printf("Hello, I'm Akemi Martinez!\n");
     printf("Major is Computer Science!\n");
     printf("Minor is undecided\n");
+    printf("I am the partner Isabella Donia\n");
+    printf("I am majoring in computer engineering");
+    
     return 0;
 }
