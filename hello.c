4a892782a2f8f64ca2fbf56fef16bd4aca1b92f3
Akemi <amartinezgutarra2@huskers.unl.edu>
2022-08-29 20:26:07 +0000

    added my minor

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 /**
  * Basic Hello World Program
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
     printf("Hello, I'm Akemi Martinez!\n");
     printf("Major is Computer Science!\n");
-
+    printf("Minor is undecided\n");
     return 0;
 }
